4efd854d72c0d3fa8df6a6f4173455045a9e4016
samhelal <93845103+samhelal@users.noreply.github.com>
2021-12-15 15:57:50 +0200

    Add files via upload

---
+++ accounttest.h
@@ -0,0 +1,43 @@
+/* (Modified Account Class) Modify class Account (Fig. 3.8) to
+provide a member function called money from an Account .
+does not exceed the withdraw that withdraws
+Ensure that the withdrawal amount Account ’s balance. If it does, the balance
+should be left unchanged and the member function should 
+display a message indicating "Withdrawal amount exceeded account balance."
+member function Modify class (Fig. 3.9) to test AccountTest withdraw */
+//class can add name ,deposite and withdraws
+#include <string>
+
+class account {
+    public :
+    account (std::string accountName , int accountBalance):
+    name (accountName){
+        if(accountBalance > 0){
+            balance = balance + accountBalance ;
+        }
+    }
+    void setName( std::string accountName){
+        name = accountName ;
+    }
+    std::string getName() const {
+        return name ;
+    }
+    void deposite(int depositAmount) {
+        if(depositAmount>0){
+            balance = balance + depositAmount ;
+        }
+    }
+    void withdraw ( int withdrawAmount ){
+        if (withdrawAmount <=balance){
+            balance = balance - withdrawAmount ;
+        }
+    }
+
+    int getBalance () const {
+        return balance ;
+    }
+
+    private :
+    std::string name ;
+    int balance{0} ;
+};
